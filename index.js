2deae93aa225991cb39ca72d9ab952185f05429e
oreste-abizera <oresteabizera11@gmail.com>
2021-01-10 17:34:25 +0200

    Sending notification on Home route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const notifier = require("node-notifier");
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  notifier.notify("Hello!");
+  res.send({ message: "Notification sent" });
+});
+
 const PORT = 7000;
 app.listen(PORT, () =>
   console.log(`Node js server is running on port ${PORT}`)
